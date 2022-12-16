dd2d003cea2eb0c16f59b6c308432a5e612366d3
Henry Bridge <hbridge@oldfashionedsw.com>
2022-12-16 16:13:25 -0500

    Initial Commit

---
+++ LoadFeed/LoadFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoadFeedApp.swift
+//  LoadFeed
+//
+//  Created by Henry Bridge on 12/16/22.
+//
+
+import SwiftUI
+
+@main
+struct LoadFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
